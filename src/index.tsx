ddc28ecc2a80e5f2dc3f1932869b1b4161f09cf9
xowoony <trumanblack@hanmail.net>
2023-03-30 16:40:18 +0900

    App.js, index.js 다시 옮김

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
 
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "rgb(0,0,0)",
+};
+
+const lightTheme = {
+  textColor: "rgb(0,0,0)",
+  backgroundColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
