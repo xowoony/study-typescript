6f144d123764910f85fc8a8dcdea60c036926a46
xowoony <trumanblack@hanmail.net>
2023-03-30 17:12:55 +0900

    typescript 시작을 위한 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import React from 'react';
 import styled from "styled-components";
-
-// Title 컴포넌트
-// index.js에서 만들어준 theme textColor에 접근해 볼 수 있다.
-const Title = styled.h1`
-  color: ${(props) =>
-    props.theme.textColor}; // props.theme.property이름 이라 작성해주면 된다.
-`;
-
-const Wrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "rgb(0,0,0)",
-};
-
-const lightTheme = {
-  textColor: "rgb(0,0,0)",
-  backgroundColor: "whitesmoke",
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -19,8 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
