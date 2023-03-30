40169e8c655f07dfe74c12c7d64b33c1ed5a0b35
xowoony <trumanblack@hanmail.net>
2023-03-30 18:53:57 +0900

    interface 사용의 쉬운 예

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -12,6 +12,7 @@ const Container = styled.div<ContainerProps>`
   width: 5rem;
   height: 5rem;
   background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
 `;
 
 // bgColor의 타입은 CircleProps의 오브젝트다 라고 말해주고 있는격
@@ -20,3 +21,16 @@ function Circle({ bgColor }: CircleProps) {
 }
 
 export default Circle;
+
+
+
+// interface 사용의 쉬운 예
+interface playerShape {
+  age: string;
+  name: string;
+}
+
+const introduce = (playerObj: playerShape) => `Hello ${playerObj.name} Your Name is ${playerObj.name}.`;
+
+
+// introduce function을 어디서든지 쓸 수 있게됨.
