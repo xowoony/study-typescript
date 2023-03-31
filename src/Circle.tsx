2a3332415714175979ae67bfca1a9496fdf44e3e
xowoony <trumanblack@hanmail.net>
2023-03-31 15:18:53 +0900

    argument 에서 default 값 설정 방법 (ES6 Javascript 구문)

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 // interface
 interface ContainerProps {
   bgColor: string; // CircleProps 상태랑 똑같이 만들어주어야 함.
-  borderColor?: string; // CircleProps 상태랑 똑같이 만들어주어야 함
+  borderColor: string; // CircleProps 상태랑 똑같이 만들어주어야 함
 }
 
 interface CircleProps {
   bgColor: string; // required. 필수적으로 사용해야함.
   borderColor?: string; // optional. 사용해도 되고 안해도 됨.
+  text?: string;
 }
 
 // styled-component
@@ -17,9 +18,8 @@ const Container = styled.div<ContainerProps>`
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
   border: 0.5rem solid ${(props) => props.borderColor};
-   // css에서 borderColor는 optional 상태가 아니다. optional 표시는 interface에서!
+  // css에서 borderColor는 optional 상태가 아니다. optional 표시는 interface에서!
 `;
-
 
 // Circle Props
 // bgColor의 타입은 CircleProps의 오브젝트다 라고 말해주고 있는격
@@ -27,9 +27,12 @@ const Container = styled.div<ContainerProps>`
 // borderColor는 optional이다. 위 interface에서 그렇게 정의해줬기 때문
 // borderColor={borderColor ?? "yellow"} 의 뜻
 // 사용자가 입력한 color를 따르겠지만, undefined면 yellow로 설정하겠다는 뜻.
-function Circle({ bgColor, borderColor }: CircleProps) {
-  return <Container bgColor={bgColor} borderColor={borderColor ?? "yellow"} />;
-
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
