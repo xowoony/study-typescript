558c208208e476f2a62799946341b61c2d17c7c8
xowoony <trumanblack@hanmail.net>
2023-04-01 16:53:49 +0900

    form 구현, form에 타입 적용하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,31 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // event를 열어보자
-    // currentTarget의 value를 얻어옴
-    // 그리고 setValue를 해줌 => 입력한 내용이 현재 value가 됨
 
-    // 타입스크립트가 현재 setValue는 string을 받아야 한다는 것을 검사하고 있고
-    // 이 onChange 이벤트는 밑에 type="text" 인 input에 의해 만들어 졌으며
-    // value를 hover 해보면 const value: string 이 뜨는 것으로 보아 타입스크립트는 이미 알고 있는 것이다.
-    const {currentTarget: { value }} = event;
+
+
+  // onChange
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event; //  ES6 문법
     setValue(value);
   };
 
+
+  // onSubmit
+  // onSubmit 또한 React.FormEvent 이므로 적어주고 form이 이벤트를 발생할 것이기 때문에
+  // HTMLFormElement를 작성해주면 된다.
+  // <>안에는 어떤 element가 이벤트를 발생시키는지 생각해보고 그걸 적어주면 된다.
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // 새로고침 방지
+    console.log("Hello", value); // value 는 user의 이름이다
+  };
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         {/* input의 value를 state와 연결하기 */}
         <input
           type="text"
