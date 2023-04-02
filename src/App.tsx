6b7807e878f9b1e4a2bf87a5eb0b44f633554425
xowoony <trumanblack@hanmail.net>
2023-04-02 16:27:41 +0900

    연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import styled from "styled-components";
 
-// Container 컴포넌트
+interface DummyProps {
+  text: string;
+}
+
+// Container
 const Container = styled.div`
   background-color: ${(props) => props.theme.bgColor};
 `;
-
-// H1 컴포넌트
-const H1 = styled.div`
+// H1
+const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+// Dummy
+function Dummy({ text }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
   return (
     <Container>
-      <H1>Protected</H1>
+      <Dummy text="dd" />
+      <button onClick={onClick}>Click me</button>
     </Container>
   );
 }
